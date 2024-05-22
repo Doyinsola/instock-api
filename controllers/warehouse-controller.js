2d01aaf0915977b5f324e354c3c5dd86fe2b9644
Doyinsola Bode-Fakunle <doyinfakunle@yahoo.com>
2024-05-22 18:16:13 -0400

    added route to get inventories for a particular warehouse

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -10,6 +10,28 @@ const index = async (_req, res) => {
   }
 };
 
+const inventories = async (req, res) => {
+  try {
+    const validWarehouse = await knex("warehouses").where({
+      id: req.params.id,
+    });
+
+    if (validWarehouse.length === 0) {
+      res.status(404).json({ message: "Warehouse not found" });
+    } else {
+      const inventories = await knex("warehouses")
+        .join("inventories", "inventories.warehouse_id", "warehouses.id")
+        .where({ warehouse_id: req.params.id });
+
+      res.status(200).json(inventories);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("Error retrieving Warehouse's inventories");
+  }
+};
+
 module.exports = {
   index,
+  inventories,
 };

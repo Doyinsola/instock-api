08b49afba8e00b5ed90d5492d607998d4217cdcf
Doyinsola Bode-Fakunle <doyinfakunle@yahoo.com>
2024-05-22 17:10:29 -0400

    added API route to get all inventories

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const CORS = require("cors");
 require("dotenv").config();
-const warehousesRoutes = require("./routes/warehouses");
 
 const app = express();
 const PORT = process.env.PORT || 8081;
 
 app.use(CORS());
 app.use(express.json());
-app.use("/", warehousesRoutes);
+
+const warehousesRoutes = require("./routes/warehouses");
+app.use("/api/warehouses", warehousesRoutes);
+
+const inventoriesRoutes = require("./routes/inventories");
+app.use("/api/inventories", inventoriesRoutes);
 
 app.listen(PORT, () => {
   console.log(`Instock api server is running at: http://localhost:${PORT}`);
--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const warehouseController = require("../controllers/warehouse-controller");
 
-router.route("/api/warehouses").get(warehouseController.index);
+router.route("/").get(warehouseController.index);
 
 module.exports = router;

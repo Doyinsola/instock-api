3b3d8c40199cce20fdb467c4502ff126c2ed4d67
Brittany Barnard <brittanyabarnard@gmail.com>
2024-05-27 03:24:17 -0400

    adding getItemById

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -10,6 +10,24 @@ const index = async (_req, res) => {
   }
 };
 
+const getItemById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const item = await knex("inventories").where({ id }).first();
+
+    if (!item) {
+      return res.status(404).json({ message: "Item not found" });
+    }
+
+    res.status(200).json(item);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("Error retrieving item");
+  }
+};
+
 module.exports = {
   index,
+  getItemById,
 };
--- routes/inventories.js
+++ routes/inventories.js
@@ -3,4 +3,6 @@ const inventoryController = require("../controllers/inventory-controller");
 
 router.route("/").get(inventoryController.index);
 
+router.route("/:id").get(inventoryController.getItemById);
+
 module.exports = router;

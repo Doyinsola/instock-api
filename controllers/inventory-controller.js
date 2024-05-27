15995b99341ca0a4197838a3eccca1d79168b4b1
Norah <norah618@outlook.com>
2024-05-26 22:43:27 -0400

    Set up inventory route

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -1,2 +1,28 @@
 const knex = require('knex')(require('../knexfile'));
 
+
+
+function isValidPhoneNumber(contact_phone) {
+    const phonePattern = /^\+1 \(\d{3}\) \d{3}-\d{4}$/;
+    return phonePattern.test(contact_phone);
+  }
+  
+function isValidEmail(contact_email) {
+if (!contact_email.includes('@') || !contact_email.includes('.')) {
+    return false;
+}
+
+if (contact_email.lastIndexOf('.') <= contact_email.indexOf('@')) {
+    return false;
+}
+
+const atIndex = contact_email.indexOf('@');
+if (contact_email[atIndex - 1] === ' ' || contact_email[atIndex + 1] === ' ') {
+    return false;
+}
+
+if (atIndex === 0 || contact_email.lastIndexOf('.') === contact_email.length - 1) {
+    return false;
+}
+return true;
+}
--- routes/inventories.js
+++ routes/inventories.js
@@ -1,2 +1,13 @@
 const router = require("express").Router();
-const warehouseController = require("../controllers/inventory-controller");
+const inventoryController = require("../controllers/inventory-controller");
+
+router
+    .route("/")
+    .get(warehouseController.index)
+
+
+// router
+//     .route("/:id")
+//     .delete(warehouseController.deleteWarehouse);
+
+module.exports = router;
